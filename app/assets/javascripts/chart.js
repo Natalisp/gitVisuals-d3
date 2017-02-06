f22f79528d94da6d552f79e06b3b617d6b0c27ae
Natalisp <nathalipat@gmail.com>
2017-02-05 21:29:35 -0500

    add nvd3 library Bar chart

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -13,125 +13,213 @@ function renderCharts(repos) {
   }).length;
   }
 
-  var ctx = document.getElementById("barChart");
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        datasets: [{
-            label: 'Repos Created',
-            data: reposCount,
-            backgroundColor: "#ca4444",
-            borderColor: "#fff",
-            borderWidth: 3,
-            fontSize: 30
-        }]
-    },
-    options: {
-      responsive: true,
-      title: 'REPOS',
-      tooltips: {
-        titleFontSize: 0,
-        bodyFontSize: 35,
-        callbacks: {
-          label: function(tooltipItems, data) {
-              return tooltipItems.yLabel + ' repos created in ' + tooltipItems.xLabel ;
-          }
-      }
-      },
-        scales: {
-          gridLines: {
-            lineWidth: 3
-          },
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true,
-                    fontSize: 30
-                }
-              }],
-            xAxes: [{
-            ticks: {
-                fontSize: 30,
-                display: true
+console.log(reposCount);
+
+
+function myData(repos) {
+
+    return [
+        {
+            key: "Series #1",
+            values:
+            [
+              {
+                "label" : "January",
+                "value" : repos[0]
               },
-            barPercentage: 1
-        }]
-      },
-      legend: {
-          position: 'top',
-          labels: {
-            padding: 15,
-            fontSize: 30
-          }
-      }
-    }
-});
+              {
+                "label" : "February",
+                "value" : repos[1]
+              },
+              {
+                "label" : "March",
+                "value" : repos[2]
+              },
+              {
+                "label" : "April",
+                "value" : repos[3]
+              },
+              {
+                "label" : "May",
+                "value" : repos[4]
+              },
+              {
+                "label" : "June",
+                "value" : repos[5]
+              },
+              {
+                "label" : "July",
+                "value" : repos[6]
+              },
+              {
+                "label" : "August",
+                "value" : repos[7]
+              },
+              {
+                "label" : "September",
+                "value" : repos[8]
+              },
+              {
+                "label" : "October",
+                "value" : repos[9]
+              },
+              {
+                "label" : "November",
+                "value" : repos[10]
+              },
+              {
+                "label" : "December",
+                "value" : repos[11]
+              },
+            ],
+            color: "#0000ff"
+        }
+    ];
+}
 
-//pieChart
+nv.addGraph(function() {
+  var width = 1600, height=600;
 
-var pie = document.getElementById("pieChart");
-var reposLanguages = repoLanguages(repos);
-var l = languageStats(reposLanguages);
+    var chart = nv.models.discreteBarChart()
+    
+    chart.x(function(d) { return d.label })
+    chart.y(function(d) { return d.value })
+    ;
 
-var myPieChart = new Chart(pie, {
-    type: 'doughnut',
-    animation:{
-        animateScale:true,
-        duration: 50000,
-        easing: 'easeInOutQuad'
-    },
-    data: {
-        labels: ["Ruby", "HTML", "JavaScript", "CSS"],
-        datasets: [{
-          backgroundColor: [
-            "#ca4444",
-            "#492540",
-            "#80d4f6",
-            "#f6ea8c"
-          ],
-          borderColor: "#fff",
-          borderWidth: 3,
-          data: [
-            l['Ruby'],
-            l['HTML'],
-            l['JavaScript'],
-            l['CSS']
-          ]
-        }]
-      },
-  options: {
-    cutoutPercentage: 40,
-        tooltips: {
-          titleFontSize: 35,
-          bodyFontSize: 35,
-          callbacks: {
-            label: function(tooltipItem, data) {
-              var dataset = data.datasets[tooltipItem.datasetIndex];
-              var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
-                return previousValue + currentValue;
-              });
-              var currentValue = dataset.data[tooltipItem.index];
-              var precentage = Math.floor(((currentValue/total) * 100)+0.5);
-              return precentage + "%";
+    d3.select("#chart svg")
+        .datum(myData(reposCount))
+        .transition().duration(500).call(chart);
+
+    nv.utils.windowResize(
+            function() {
+                chart.update();
             }
-          }
-        },
-         responsive: true,
-         legend: {
-             padding: 15,
-             position: 'top',
-             labels: {
-               fontSize: 30
-             }
-         },
-         scale: {
-           ticks: {
-             beginAtZero: true
-           },
-           reverse: false
-         }
-     }
+        );
+
+    return chart;
 });
+
+
+
+//   var ctx = document.getElementById("barChart");
+//   var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+//         datasets: [{
+//             label: 'Repos Created',
+//             data: reposCount,
+//             backgroundColor: "#ca4444",
+//             borderColor: "#fff",
+//             borderWidth: 3,
+//             fontSize: 30
+//         }]
+//     },
+//     options: {
+//       responsive: true,
+//       title: 'REPOS',
+//       tooltips: {
+//         titleFontSize: 0,
+//         bodyFontSize: 35,
+//         callbacks: {
+//           label: function(tooltipItems, data) {
+//               return tooltipItems.yLabel + ' repos created in ' + tooltipItems.xLabel ;
+//           }
+//       }
+//       },
+//         scales: {
+//           gridLines: {
+//             lineWidth: 3
+//           },
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true,
+//                     fontSize: 30
+//                 }
+//               }],
+//             xAxes: [{
+//             ticks: {
+//                 fontSize: 30,
+//                 display: true
+//               },
+//             barPercentage: 1
+//         }]
+//       },
+//       legend: {
+//           position: 'top',
+//           labels: {
+//             padding: 15,
+//             fontSize: 30
+//           }
+//       }
+//     }
+// });
+//
+// //pieChart
+//
+// var pie = document.getElementById("pieChart");
+// var reposLanguages = repoLanguages(repos);
+// var l = languageStats(reposLanguages);
+//
+// var myPieChart = new Chart(pie, {
+//     type: 'doughnut',
+//     animation:{
+//         animateScale:true,
+//         duration: 50000,
+//         easing: 'easeInOutQuad'
+//     },
+//     data: {
+//         labels: ["Ruby", "HTML", "JavaScript", "CSS"],
+//         datasets: [{
+//           backgroundColor: [
+//             "#ca4444",
+//             "#492540",
+//             "#80d4f6",
+//             "#f6ea8c"
+//           ],
+//           borderColor: "#fff",
+//           borderWidth: 3,
+//           data: [
+//             l['Ruby'],
+//             l['HTML'],
+//             l['JavaScript'],
+//             l['CSS']
+//           ]
+//         }]
+//       },
+//   options: {
+//     cutoutPercentage: 40,
+//         tooltips: {
+//           titleFontSize: 35,
+//           bodyFontSize: 35,
+//           callbacks: {
+//             label: function(tooltipItem, data) {
+//               var dataset = data.datasets[tooltipItem.datasetIndex];
+//               var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+//                 return previousValue + currentValue;
+//               });
+//               var currentValue = dataset.data[tooltipItem.index];
+//               var precentage = Math.floor(((currentValue/total) * 100)+0.5);
+//               return precentage + "%";
+//             }
+//           }
+//         },
+//          responsive: true,
+//          legend: {
+//              padding: 15,
+//              position: 'top',
+//              labels: {
+//                fontSize: 30
+//              }
+//          },
+//          scale: {
+//            ticks: {
+//              beginAtZero: true
+//            },
+//            reverse: false
+//          }
+//      }
+// });
 
 
 }
